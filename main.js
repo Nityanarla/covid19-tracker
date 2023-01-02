e8aba46d8c256a7ccbddb585b6560680cc450950
Nityanarla <92780620+Nityanarla@users.noreply.github.com>
2023-01-02 23:14:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import Home from './components/Home';
+import WelcomeScreen from './components/WelcomScreen';
+
+
+
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes: [
+        {
+            path:'/', component:WelcomeScreen
+        },
+        {
+            path:'/stats', component:Home
+        },
+        {
+            path:'/:notFound(.*)', redirect:'/'
+        }
+    ]
+})
+const app = createApp(App);
+
+
+
+app.use(router);
+
+app.mount('#app');
+
+//keep the good work going
